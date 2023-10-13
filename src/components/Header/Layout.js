da20381491ffe64861cb2b20bd16788af9bb2241
PokoenkoS <pokoenkosveta@gmail.com>
2023-10-13 11:08:13 +0300

    fixed styles Layout

--- src/components/Header/Layout.js
+++ src/components/Header/Layout.js
@@ -1,19 +1,15 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Ul, HeaderTag, NavTag, StyledNavLink, Main  } from "./Layout.styled";
+import { List, HeaderTag, StyledNavLink, Main  } from "./Layout.styled";
 
 export const Layout = () => {
   return (
     <>
       <HeaderTag>
-        <NavTag>
-                        
-          <Ul>
+          <List>
             <li><StyledNavLink to="/">Home</StyledNavLink></li>
             <li><StyledNavLink to="/movies">Movies</StyledNavLink></li>
-          </Ul>
-        </NavTag>
-               
+          </List>
       </HeaderTag>
       <Main>
         <Suspense fallback={<div>Loading...</div>}>
--- src/components/Header/Layout.styled.jsx
+++ src/components/Header/Layout.styled.jsx
@@ -1,41 +1,27 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-export const NavTag = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 60px;
-    font-size: 30px;
-    font-weight: 900;
-    height: 100%;
-    padding-left: 10px;
-    padding-right: 10px;
-    box-sizing: border-box;
-    flex-direction: row-reverse;
-`;
-
 export const HeaderTag = styled.header`
-    width: 100%;
-    position: fixed;
-    top: 0;
-    z-index: 10;
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    background-color: #2e0b82;
-    border-bottom: 1px solid gray;
-    box-shadow: 2px 3px 5px lightgrey;
-    
-    
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  background-color: #2e0b82;
+  border-bottom: 1px solid gray;
+  box-shadow: 2px 3px 5px lightgrey;
 `;
 
-export const Ul = styled.ul`
-    display: flex;
-    gap: 20px;
-    list-style: none;
+export const List = styled.ul`
+  width: 100%;
+  font-size: 30px;
+  font-weight: 900;
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+  list-style: none;
 `;
 
 export const StyledNavLink = styled(NavLink)`
